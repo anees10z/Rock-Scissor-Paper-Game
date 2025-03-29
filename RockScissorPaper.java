71774bcba320c4627ab818f98a01ded26b40fbe3
Md Anees Khan <khanmdanis145@gmail.com>
2025-03-29 12:43:06 +0530

    Update RockScissorPaper.java

--- RockScissorPaper.java
+++ RockScissorPaper.java
@@ -3,13 +3,13 @@ import java.util.Random;
 import java.util.*;
 /* Conditions of game
             computer-->
-            snake[0]   water[1]    gun[2]
+            Rock[0]   Scissor[1]    Paper[2]
 user !
-snake[0]     draw         win       loose
+Rock[0]     draw         win       loose
 
-water[1]     loose        draw       win   
+Scissor[1]     loose        draw       win   
 
-gun[2]        win         loose      draw
+Paper[2]        win         loose      draw
 */
 
 class RockScissorPaper {
@@ -23,8 +23,8 @@ class RockScissorPaper {
                 { "Win", "Loose", "Draw" }
         };
         // Display Game
-        System.out.println("\n\n------- Snake --- Water --- Gun -------\n");
-        System.out.println("    0 = Snake   1 = Water   2 = Gun   \n");
+        System.out.println("\n\n------ Rock --- Scissor --- Paper ------\n");
+        System.out.println("    0 = Rock   1 = Scissor   2 = Paper   \n");
         // user input 0 - 2
         System.out.print("Enter Your Choice : ");
         int user = sc.nextInt();
